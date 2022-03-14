bb6f064b62e0c419807fcf4c6dab0ca6e63ca0e8
João Marcos <jmarcossferreira@hotmail.com>
2022-03-13 21:18:46 -0300

    Melhorando exibição de tela inicial na página da wiki

--- src/components/pages/Motorcycles/EmptyDetails.jsx
+++ src/components/pages/Motorcycles/EmptyDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from '../../../assets/search.svg?component';
 
 const EmptyDetails = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center h-full">
-      (Selecione uma marca para var a listagem de motos)
+    <div className="d-flex flex-column justify-content-center align-items-center h-full">
+      <Search />
+      <p className="mt-3">(Selecione uma marca para var a listagem de motos)</p>
     </div>
   );
 };
